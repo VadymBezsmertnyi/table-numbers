033a2960406c20c11fa241ea2a62901b4865b22f
VadymBezsmertnyi <vados199368@gmail.com>
2022-12-08 13:16:30 +0200

    add settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-
-function App() {
+const App = () => {
+  console.log(process.env.PUBLIC_URL);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit fsdfsdf<code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit fsdfsdf<code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </HashRouter>
   );
-}
+};
 
 export default App;
