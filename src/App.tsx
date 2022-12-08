fae4bba3421b352cde6fc9d2343b29ce91fa4c87
VadymBezsmertnyi <vados199368@gmail.com>
2022-12-08 13:19:57 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,23 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  console.log(process.env.PUBLIC_URL);
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit fsdfsdf<code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </HashRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 };
 
